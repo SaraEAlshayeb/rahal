de3d7f44aee7404edcce3a12c0f03ab7aea5dd86
sarah alshallali <s202167710@kfupm.edu.sa>
2025-04-14 01:44:04 +0300

    sara commit2

---
+++ src/Login.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import './Login.css';
+import Background from './background.png';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      alert('All fields are required.');
+      return;
+    }
+  
+    navigate('/home'); 
+  };
+  
+
+  return (
+    <div
+      className="login-wrapper"
+      style={{
+        backgroundImage: `url(${Background})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+    
+
+     
+      <h1 className="login-title">Welcome To Rahal</h1>
+      <p className="subtitle">Your most convenient ride</p>
+
+  
+      
+      <div className="login-form">
+  <div className="form-group">
+    <label htmlFor="email">Email:</label>
+    <input
+      id="email"
+      type="email"
+      placeholder="Ex.example@hotmail.com"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      required
+    />
+  </div>
+
+  <div className="form-group">
+    <label htmlFor="password">Password:</label>
+    <input
+      id="password"
+      type="password"
+      placeholder="Enter your password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      required
+    />
+  </div>
+</div>
+
+    
+      <div className="button-container">
+        <button onClick={handleLogin}>Login</button>
+        <p className="register-link">
+          Don’t have an account? <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
