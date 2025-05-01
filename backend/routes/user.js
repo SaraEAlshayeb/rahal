55c0ce9cb30f71e44ab198777b8c5febdfb0c02a
FarahMohammedAlmutairi <s202159190@kfupm.edu.sa>
2025-05-01 16:05:19 +0300

    Farah

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers, suspendUser } = require("../controllers/userController");
+const { getAllUsers, suspendUser,registerUser, getUserByEmail} = require("../controllers/userController");
 
 router.get("/", getAllUsers);                  // GET /api/users
 router.put("/suspend", suspendUser);           // PUT /api/users/suspend
+router.get("/:email", getUserByEmail);
+router.post("/register", registerUser); 
 
 module.exports = router;
